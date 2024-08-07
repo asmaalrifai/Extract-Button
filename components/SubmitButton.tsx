f09e625364588b78341f160310ad8857d6d1ef05
asmaalrifai <asma.o.alrifai@gmail.com>
2024-08-07 15:09:09 +0300

    Edit 1.5

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 interface ButtonProps {
   isLoading: boolean;
@@ -15,20 +14,6 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
       disabled={isLoading}
       className={className ?? "shad-primary-btn w-full"}
     >
-      {isLoading ? (
-        <div className="flex items-center gap-4">
-          <Image
-            src="/assets/icons/loader.svg"
-            alt="loader"
-            width={24}
-            height={24}
-            className="animate-spin"
-          />
-          Loading...
-        </div>
-      ) : (
-        children
-      )}
     </Button>
   );
 };
